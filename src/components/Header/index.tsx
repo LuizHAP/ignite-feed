d67f29150150292728001c135393f6479058b61e
DESKTOP-MLV3BLB\luizg <lpansarini@gmail.com>
2023-02-15 10:37:50 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import styles from './index.module.css'
+
+import igniteLogo from '../../assets/ignite-logo.svg';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Logotipo do Ignite" />
+    </header>
+  )
+}
