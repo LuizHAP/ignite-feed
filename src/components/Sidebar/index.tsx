9d488c9e2352d6cba404e5975cacf18459f82c65
Luiz Pansarini <lpansarini@gmail.com>
2023-02-15 11:58:35 -0300

    feat: finish sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react'
 import styles from './index.module.css'
 
 export default function Sidebar() {
@@ -6,12 +7,14 @@ export default function Sidebar() {
       <img className={styles.cover} src="https://images.unsplash.com/photo-1602992708529-c9fdb12905c9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/20971507?v=4" alt="" />
         <strong>Luiz Pansarini</strong>
-        <p>Front-end Developer</p>
+        <span>Front-end Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
