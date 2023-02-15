db5a605cf7d915411c71928aa4d97779c910e3f7
Luiz Pansarini <lpansarini@gmail.com>
2023-02-15 17:18:36 -0300

    fix: styling posts

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,15 +1,37 @@
 import styles from './index.module.css'
 
-type HeaderProps = {
+type PostProps = {
   author: string
   content: string
 }
 
-export default function Post({ author, content }: HeaderProps) {
+export default function Post({ author, content }: PostProps) {
   return (
-    <div>
-      <strong>{author}</strong>
-      <p>{content}</p>
-    </div>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img src="https://github.com/luizhap.png" />
+          <div className={styles.authorInfo}>
+            <strong>{author}</strong>
+            <p>{content}</p>
+          </div>
+        </div>
+
+        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:38">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>
+          <p>Fala galeraa 👋</p>
+          <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+          <p><a href="">jane.design/doctorcare</a></p>
+          <p>
+            <a href="">#novoprojeto</a>{' '}
+            <a href="">#nlw</a>{' '}
+            <a href="">#rocketseat</a>
+          </p>
+        </p>
+      </div>
+    </article>
   )
 }
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -7,7 +7,7 @@ export default function Sidebar() {
       <img className={styles.cover} src="https://images.unsplash.com/photo-1602992708529-c9fdb12905c9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/20971507?v=4" alt="" />
+        <img className={styles.avatar} src="https://github.com/luizhap.png" alt="" />
         <strong>Luiz Pansarini</strong>
         <span>Front-end Developer</span>
       </div>
